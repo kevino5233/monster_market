59767e2230e519d3ae90b02ae3e62a820da3a8b8
Kevin Sun <kevinosu@cs.utexas.edu>
2015-08-22 13:48:42 -0500

    Player walk animation.

--- lot_state.js
+++ lot_state.js
@@ -16,8 +16,11 @@ var lot_state = {
 	game.add.sprite(0, 0, "lot_bg");
 	this.player = game.add.sprite(400, 300, "player");
 	this.player.base_velocity = 200;
-	this.player.animations.add("idle", [0]);
-	this.player.animations.add("walk", [9, 10, 11, 12], 4, true);
+	this.player.right = true;
+	this.player.animations.add("idle_right", [0]);
+	this.player.animations.add("idle_left", [8]);
+	this.player.animations.add("walk_right", [16, 17, 18, 19], 4, true);
+	this.player.animations.add("walk_left", [24, 25, 26, 27], 4, true);
 	game.physics.enable(this.player);
 
 	this.cursor = game.input.keyboard.createCursorKeys();
@@ -43,11 +46,26 @@ var lot_state = {
 	    this.player.body.velocity.x = 0;
 	}
 
-	if (this.player.body.velocity.x || this.player.body.velocity.y){
-	    this.player.animations.play("walk");
+	var anim_state = "idle_left";
+	var player_x_vel = this.player.body.velocity.x;
+	var player_y_vel = this.player.body.velocity.y;
+	if (player_x_vel || player_y_vel){
+	    if ((!player_x_vel && this.player.right) || player_x_vel > 0){
+		anim_state = "walk_right";
+		this.player.right = true;
+	    } else {
+		anim_state = "walk_left";
+		this.player.right = false;
+	    }
 	} else {
-	    this.player.animations.play("idle");
+	    if (this.player.right){
+		anim_state = "idle_right";
+		this.player.right = true;
+	    } else {
+		this.player.right = false;
+	    }
 	}
+	this.player.animations.play(anim_state);
 
 	UpdateCamera(game.camera, this.player);
     },
