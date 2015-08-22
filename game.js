6f2c7c15f27d0b532f48c17c927eae138187c226
Kevin Sun <kevinosu@cs.utexas.edu>
2015-08-21 23:09:06 -0500

    Initial commit.

---
+++ game.js
@@ -0,0 +1,22 @@
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, loadUpdate: loadUpdate, loadRender: loadRender, create: create, update: update, render: render, resize: resize, shutdown: shutdown});
+
+function preload() {
+}
+
+function loadUpdate() {
+}
+
+function loadRender() {
+}
+
+function create() {
+}
+
+function update() {
+}
+
+function resize(){
+}
+
+function shutdown(){
+}
