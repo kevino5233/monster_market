84be5403e6a2d823b222c1d4a5bf204c407288a7
Kevin Sun <kevinosu@cs.utexas.edu>
2015-08-23 23:27:57 -0500

    Begin cashier level

---
+++ cashier_state.js
@@ -0,0 +1,23 @@
+var cashier_state = {
+    preload: function(){
+    },
+
+    loadUpdate: function(){
+    },
+
+    loadRender: function(){
+    },
+
+    create: function(){
+    },
+
+    update: function(){
+    },
+
+    resize: function(){
+    },
+
+    shutdown: function(){
+    }
+}
+
